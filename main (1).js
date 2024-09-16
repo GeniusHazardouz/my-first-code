24959dd2c2c707442b829bb3dc66fc8d24412af4
GeniusHazardouz <155668977+GeniusHazardouz@users.noreply.github.com>
2024-09-16 14:15:40 +0100

    Add files via upload

---
+++ main (1).js
@@ -0,0 +1,8 @@
+function sayILoveYou() {
+    let name = prompt('Enter your name:');
+
+    console.log('I Love You, ' + name + '!');
+}
+
+// Press the play button to see the result.
+sayILoveYou();
